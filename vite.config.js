d3666927c16adb6a556027550501ff19c72dc66a
ridamukhtr <ridamukhtar638@gmail.com>
2025-09-29 12:20:10 +0500

    deploy issue set

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: './', // This is the most important line for deployment
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
+    }
 })
