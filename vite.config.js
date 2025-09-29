7ccd763de40c865fd677a5468af8661b2ef1a12f
ridamukhtr <ridamukhtar638@gmail.com>
2025-09-29 12:30:12 +0500

    gitignore update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: './', // This is the most important line for deployment
+    base: './',
     build: {
         outDir: 'dist',
         assetsDir: 'assets'
+    },
+    optimizeDeps: {
+        include: ['bootstrap'] // if using Bootstrap
+    },
+    define: {
+        // Global settings if needed
+        'process.env': {}
     }
 })
